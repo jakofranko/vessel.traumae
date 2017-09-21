3b38efd3dc31f0f1ed8c979b4b2b0de0989c2806
Jake Franklin <jacob.d.franklin@gmail.com>
2017-09-21 05:26:03 +0000

    Beginning work on helpers for the aebeth

--- objects/aebeth.rb
+++ objects/aebeth.rb
@@ -2,8 +2,10 @@ class Aebeth
     
     def initialize path
        
-       @h = Memory_Hash.new("aebeth", path)
-       print @h.inspect
+       load_any(path, "aeth")
+       
+       @h = Memory_Hash.new("aebeth", path).to_h("aeth")
+    #   print @h.inspect
        
     end
         
--- objects/aeth.rb
+++ objects/aeth.rb
@@ -1,15 +1,30 @@
 class Aeth
     
-    attr_accessor :name, :data
+    attr_accessor :root_meaning, :data
     
     def initialize name, data
         
-        @name = name.capitalize
-        @data = data
+        @root_meaning = name.capitalize
+        @aeth_data = data
         
         print data.inspect
     end
     
+    def capitalization cap
+
+       vowel = @aeth_data["vowel"]
+
+       case cap
+       when 0
+           @aeth_data["root"] >> vowel
+        when 1
+            @aeth_data["median"]["root"] >> vowel
+        when 2
+            @aeth_data["opposite"]["root"] >> vowel
+        end
+
+    end
+
     def capitalizations
         
     end
