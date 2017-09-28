906e82e62076054d0b68fcb9c4550e782058e8a5
Jake Franklin <jacob.d.franklin@gmail.com>
2017-09-28 05:04:02 +0000

    Updated test

--- actions/test.rb
+++ actions/test.rb
@@ -1,24 +1,25 @@
 class ActionTest
-   
+
    include Action
-    
+
    def initialize q = nil
-      
+
       super
-       
+
       @name = "Test"
       @docs = "Test the Traumae actions and objects"
-       
+
    end
-   
+
    def act q = nil
-       
+
        load_folder("#{@host.path}/objects/*")
-       
+
        @aebeth = Aebeth.new(@host.path)
-       
-       @aebeth.list
-       
+       @documentation = Documentation.new(@host.path).to_s
+
+      #  @aebeth.list
+
    end
-    
+
 end
