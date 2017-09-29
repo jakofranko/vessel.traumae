feed6a70e1b7fc2de66a8c1788913765edb04e1e
Jake Franklin <jacob.d.franklin@gmail.com>
2017-09-29 05:36:18 +0000

    Documentation can now support runes

--- objects/documentation.rb
+++ objects/documentation.rb
@@ -1,34 +1,58 @@
 require_relative "../../vessel.oscean/objects/index.rb"
 class Documentation
 
-   def initialize path
+    RUNES = ['$', '#']
 
-       @h = Memory_Hash.new("documentation", path).to_h
-        print @h.inspect
-   end
+    def initialize path
 
-   def to_s
+        @h = Memory_Hash.new("documentation", path).to_h
+        @index = Index.new
 
-        index = Index.new
+    end
+
+    def to_s
+
         html = ""
         @h.each do |heading, paragraphs|
             if heading == "SOURCES" then next end
 
-            html += index.add(:root, heading.downcase.to_sym)
+            html += @index.add(:root, heading.downcase.to_sym)
             paragraphs.each do |p|
-                # TODO: handle runes
-                html += "<p>#{p}</p>"
+                rune, spell = get_rune(p)
+
+                if rune
+                    html += cast_spell(rune, spell, heading)
+                else
+                    html += "<p>#{p}</p>"
+                end
             end
         end
 
-        print index.inspect
-        return index.to_s(true) + html
-   end
+        return @index.to_s(true) + html
+    end
 
-   def rune s
+    def get_rune s
 
-       return s[0, 1]
+        rune = s[0, 1]
+        spell = s[1..-1]
+        if RUNES.include? rune
+            return rune, spell
+        else
+            return nil
+        end
 
-   end
+    end
 
+    def cast_spell rune, spell, heading = nil
+
+        result = ""
+        case rune
+        when "$"
+            result += $nataniev.answer(spell)
+        when "#"
+            result += @index.add(heading.to_sym, spell.to_sym)
+        end
+
+        return result
+    end
 end
