f558ab9a74e2357afe16d81abc5655dcf2ae45a4
Jake Franklin <jacob.d.franklin@gmail.com>
2017-11-06 03:59:18 +0000

    Updated the test

--- actions/test.rb
+++ actions/test.rb
@@ -15,11 +15,18 @@ class ActionTest
 
        load_folder("#{@host.path}/objects/*")
 
-       @aebeth = Aebeth.new(@host.path)
-      #  @documentation = Documentation.new(@host.path).to_s
-      #  @ehrivevnv = Ehrivevnv.new(@host.path)
-      #  puts @ehrivevnv.to_letters "Ki Saxan semke"
-       puts @aebeth.to_letters("pi vabi so ti", "recit")
+      @aebeth = Aebeth.new(@host.path)
+      puts @aebeth.list
+      # @documentation = Documentation.new(@host.path).to_s
+      # @ehrivevnv = Ehrivevnv.new(@host.path)
+      # puts @ehrivevnv.to_letters "Ki Saxan semke"
+      # puts @aebeth.to_letters("pi vabi so ti", "recit")
+      @dictionaery = Dictionaery.new(@host.path)
+      # puts @dictionaery.traumae_to_english
+      # puts @dictionaery.english_to_traumae
+      # puts @dictionaery.to_traumae("race skeleton peak experience bob's your uncle")
+      # puts @dictionaery.to_english("Tiso lado xoka to")
+      puts @dictionaery.literal("Tiso")
 
    end
 
