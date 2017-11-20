08f98a7949efe0436326c0389169cb1dc72652be
Jake Franklin <jacob.d.franklin@gmail.com>
2017-11-20 05:29:20 +0000

    Added a method for generating the letters json string

--- actions/generate.rb
+++ actions/generate.rb
@@ -7,7 +7,7 @@ class ActionGenerate
         super
 
         @name = "Generate"
-        @docs = "Generate the Traumae Dictionaery memory"
+        @docs = "Generate the Traumae Dictionaery memory and letters hash"
 
     end
 
@@ -15,16 +15,27 @@ class ActionGenerate
 
         load_folder("#{@host.path}/objects/*")
 
+        @ehrivevnv = Ehrivevnv.new(@host.path)
         @dict = Dictionaery.new(@host.path).english_to_traumae
         @english_length = max_word_length
 
-        memory = to_memory
+        if(params == "memory")
+            memory = to_memory
 
-        # Put the memory to stdout for creating the file
-        puts memory
+            # Put the memory to stdout for creating the file
+            puts memory
 
-        # And return it in case something else must be done
-        return memory
+            # And return it in case something else must be done
+            return memory
+        elsif(params == "letters")
+            letters = to_letters
+
+            # for stdout
+            puts letters.to_json
+
+            # And return...
+            return letters
+        end
 
     end
 
@@ -44,6 +55,24 @@ class ActionGenerate
 
         return dictionary
 
+    end
+
+    def to_letters
+
+        letters = {}
+        alts = ['letter', 'alt', 'septambres']
+        dict = @ehrivevnv.list
+        dict.each do |aeth, english|
+            alts.each do |alt|
+                if !letters[alt] then letters[alt] = {} end
+                c = @ehrivevnv.to_letters(aeth.downcase, alt == 'letter' ? nil : alt, false)
+                if !letters[alt][c] then letters[alt][c] = {} end
+                letters[alt][c][:traumae] = aeth
+                letters[alt][c][:english] = english
+            end
+        end
+
+        return letters
     end
 
     private
